a47c75544e526183de0fe8d15ed77929d2b3c876
lmk <lmk@sptek.co.kr>
2020-12-15 15:33:51 +0900

    first

---
+++ iworking/Form.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace iworking
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dtLastInput = new System.Windows.Forms.DateTimePicker();
+            this.labelLastInput = new System.Windows.Forms.Label();
+            this.timerNoInput = new System.Windows.Forms.Timer(this.components);
+            this.labelStart = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // dtLastInput
+            // 
+            this.dtLastInput.Enabled = false;
+            this.dtLastInput.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtLastInput.Location = new System.Drawing.Point(116, -6);
+            this.dtLastInput.Name = "dtLastInput";
+            this.dtLastInput.ShowUpDown = true;
+            this.dtLastInput.Size = new System.Drawing.Size(134, 21);
+            this.dtLastInput.TabIndex = 0;
+            this.dtLastInput.Visible = false;
+            // 
+            // labelLastInput
+            // 
+            this.labelLastInput.AutoSize = true;
+            this.labelLastInput.Location = new System.Drawing.Point(12, 0);
+            this.labelLastInput.Name = "labelLastInput";
+            this.labelLastInput.Size = new System.Drawing.Size(67, 13);
+            this.labelLastInput.TabIndex = 1;
+            this.labelLastInput.Text = "Last Time";
+            this.labelLastInput.Visible = false;
+            // 
+            // timerNoInput
+            // 
+            this.timerNoInput.Interval = 1000;
+            this.timerNoInput.Tick += new System.EventHandler(this.TimerNoInput_Tick);
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Location = new System.Drawing.Point(12, 27);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(69, 13);
+            this.labelStart.TabIndex = 3;
+            this.labelStart.Text = "Start Time";
+            // 
+            // dtStart
+            // 
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtStart.Location = new System.Drawing.Point(116, 21);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.ShowUpDown = true;
+            this.dtStart.Size = new System.Drawing.Size(134, 21);
+            this.dtStart.TabIndex = 2;
+            // 
+            // labelEnd
+            // 
+            this.labelEnd.AutoSize = true;
+            this.labelEnd.Location = new System.Drawing.Point(12, 54);
+            this.labelEnd.Name = "labelEnd";
+            this.labelEnd.Size = new System.Drawing.Size(65, 13);
+            this.labelEnd.TabIndex = 5;
+            this.labelEnd.Text = "End Time";
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtEnd.Location = new System.Drawing.Point(116, 48);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.ShowUpDown = true;
+            this.dtEnd.Size = new System.Drawing.Size(134, 21);
+            this.dtEnd.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(273, 89);
+            this.Controls.Add(this.labelEnd);
+            this.Controls.Add(this.dtEnd);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.dtStart);
+            this.Controls.Add(this.labelLastInput);
+            this.Controls.Add(this.dtLastInput);
+            this.Name = "FormMain";
+            this.Text = "i working";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtLastInput;
+        private System.Windows.Forms.Label labelLastInput;
+        private System.Windows.Forms.Timer timerNoInput;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Label labelEnd;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+    }
+}
+
---
+++ iworking/Form.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Hook;
+
+namespace iworking
+{
+    public partial class FormMain : Form
+    {
+        private Point mousePosition;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            dtStart.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 9, 0, 0);
+            dtEnd.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 18, 0, 0);
+
+            KeyboardHook.KeyDown += InputKeyboard;
+            KeyboardHook.KeyUp += InputKeyboard;
+            MouseHook.MouseDown += InputMouse;
+            MouseHook.MouseUp += InputMouse;
+            MouseHook.MouseMove += InputMouse;
+            MouseHook.MouseScroll += InputMouseScroll;
+
+            KeyboardHook.HookStart();
+            if (!MouseHook.HookStart())
+            {
+                MessageBox.Show("Mouse hook failed");
+            }
+
+            timerNoInput.Start();
+
+            mousePosition = Cursor.Position;
+        }
+
+        private void DoInput()
+        {
+            dtLastInput.Value = DateTime.Now;
+        }
+
+        private bool InputMouse(MouseEventType type, int x, int y)
+        {
+            mousePosition.X = x;
+            mousePosition.Y = y;
+            DoInput();
+            return true;
+        }
+
+        private bool InputKeyboard(int vkCode)
+        {
+            DoInput();
+            return true;
+        }
+
+        private bool InputMouseScroll(MouseScrollType type)
+        {
+            DoInput();
+            return true;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            dtLastInput.Value = DateTime.Now;
+        }
+
+        private void TimerNoInput_Tick(object sender, EventArgs e)
+        {
+
+            TimeSpan ts = DateTime.Now.Subtract(dtLastInput.Value);
+
+            // 60초 동안 입력이 없으면 동작
+            if (ts.TotalSeconds >= 60)
+            {
+                int start = Int32.Parse(dtStart.Value.ToString("HHmmss"));
+                int end = Int32.Parse(dtEnd.Value.ToString("HHmmss"));
+                int now = Int32.Parse(DateTime.Now.ToString("HHmmss"));
+
+                if ((start <= now) && (now <= end))
+                {
+                    SimulMouse();
+                }
+            }
+        }
+
+        private void SimulMouse()
+        {
+            MouseSimulation.MouseUp(MouseEventType.NONE, mousePosition.X - 10, mousePosition.Y - 10);
+            MouseSimulation.MouseUp(MouseEventType.NONE, mousePosition.X, mousePosition.Y);
+        }
+    }
+}
